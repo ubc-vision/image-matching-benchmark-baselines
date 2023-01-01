f1061094ff3ed4ed2f57d2eb0aa47cb303220626
Dmytro Mishkin <ducha.aiki@gmail.com>
2023-01-01 14:26:32 +0100

    added sp variants repos

---
+++ download_kp2d_weights.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir third_party/KP2D/data
+mkdir third_party/KP2D/data/models
+mkdir third_party/KP2D/data/models/kp2d/ 
+#wget https://tri-ml-public.s3.amazonaws.com/github/kp2d/models/pretrained_models.tar.gz
+mv pretrained_models.tar.gz third_party/KP2D/data/models/kp2d/
+cd third_party/KP2D/data/models/kp2d/ && tar -xzf pretrained_models.tar.gz 
