2a258c2ed91655fec768a3ee076ac73020c448c9
Dmytro Mishkin <ducha.aiki@gmail.com>
2023-01-01 17:10:51 +0100

    fix dl script

--- download_kp2d_weights.sh
+++ download_kp2d_weights.sh
@@ -2,6 +2,6 @@
 mkdir third_party/KP2D/data
 mkdir third_party/KP2D/data/models
 mkdir third_party/KP2D/data/models/kp2d/ 
-#wget https://tri-ml-public.s3.amazonaws.com/github/kp2d/models/pretrained_models.tar.gz
+wget https://tri-ml-public.s3.amazonaws.com/github/kp2d/models/pretrained_models.tar.gz
 mv pretrained_models.tar.gz third_party/KP2D/data/models/kp2d/
 cd third_party/KP2D/data/models/kp2d/ && tar -xzf pretrained_models.tar.gz 
